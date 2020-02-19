4519bad613d18a39e2b99c8c87748251710e6d78
Time_Terminal <vishesh.thanki@gmail.com>
2020-02-18 21:35:40 -0500

    Use Hooks instead of React class

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,96 +1,74 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import '../index.scss';
 import { getRandomFilter, requestCollection } from '../helpers';
 import { Piece } from "./Piece";
 import { debounce } from 'lodash';
 
-export default class Content extends Component {
-  constructor() {
-    super();
-    this.state = {
-      filter: '',
-      isLoading: false,
-    }
-  }
+const Content = () => {
+  const [filter, setFilter] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [collection, setCollection] = useState([]);
 
-  componentDidMount() {
+  // Initially set filter to one of the pre-determined "queries"
+  useEffect(() => {
     const filter = getRandomFilter();
-    this.searchCollection(filter);
+    setFilter(filter);
+    setCollection(searchCollection(filter));
+  }, []);
+
+  const searchCollection = (filter) => {
+    setIsLoading(true);
+
+    requestCollection(filter).then(data =>
+      setCollection(data)
+    )
+
+    setIsLoading(false);
   }
 
-  searchCollection(filter) {
-    console.log('hi ============');
-    this.setState({
-      filter,
-      isLoading: true
-    });
-
-    // requestCollection(filter).then(data =>
-    //   this.setState({
-    //     collection: data,
-    //     isLoading: false
-    //   })
-    // )
-  }
-
-  handleSearchChange(query) {
-    console.log('hi ============', query);
-    this.setState({
-      filter: query
-    })
-
-    this.useDebouncer(this.searchCollection, 650);
-  }
-
-  useDebouncer(func, delay = 0) {
-    const { filter } = this.state;
+  const useDebouncer = (func, delay = 0) => {
+    // useDebouncer(searchCollection, 650);
+    // filter
     const newFunc = () => { };
     return newFunc;
   }
 
-  clearSearch() {
-    this.setState({
-      filter: ''
-    })
-  }
+  console.log('filter :', filter);
+  return (
+    <div className="top-container">
+      <section className='container header-container'>
+        <h1 className='header'>The Metropolitan Museum of Art Collection</h1>
+      </section>
 
-  render() {
-    // console.log('this.state :', this.state);
-    const { collection, isLoading } = this.state;
-    return (
-      <div className="top-container">
-        <section className='container header-container'>
-          <h1 className='header'>The Metropolitan Museum of Art Collection</h1>
-        </section>
-
-        <section className='container form-container'>
-          <form className='form'>
-            <input
-              name='search'
-              type='search'
-              className='search'
-              placeholder={this.state.filter}
-              onChange={(e) => this.handleSearchChange(e.target.value)}
-            />
-            {this.state.filter !== '' &&
-              <button
-                name='clear'
-                className='clear-button'
-                onClick={(e) => this.clearSearch(e)}
-              >
-                X
-            </button>
-            }
-          </form>
-        </section>
-        <section className='container images-container'>
-          {isLoading && <p>Loading Results...</p>}
-          {collection && collection.length !== 0 && collection.map(piece =>
-            <Piece piece={piece} key={`${piece.title}-${piece.objectID}`} />
-          )
+      <section className='container form-container'>
+        <form className='form'>
+          <input
+            name='search'
+            type='search'
+            className='search'
+            placeholder={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          />
+          {filter !== '' &&
+            <button
+              name='clear'
+              className='clear-button'
+              onClick={() => setFilter('')}
+            >
+              X
+          </button>
           }
-        </section>
-      </div>
-    )
-  }
+        </form>
+      </section>
+      <section className='container images-container'>
+        {isLoading && <p>Loading Results...</p>}
+        {collection && collection.length !== 0 && collection.map(piece =>
+          <Piece piece={piece} key={`${piece.title}-${piece.objectID}`} />
+        )
+        }
+      </section>
+    </div>
+  )
 }
+
+export default Content;
